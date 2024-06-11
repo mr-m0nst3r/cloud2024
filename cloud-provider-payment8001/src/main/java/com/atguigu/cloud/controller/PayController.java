a6671933364ac2fe52e97bcfbd546a064eee0d03
mr-m0nst3r <songxinlei@gmail.com>
2024-06-11 19:05:24 +0800

    swagger

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -3,6 +3,8 @@ package com.atguigu.cloud.controller;
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
 import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -12,12 +14,14 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@Tag(name = "支付微服务模块", description = "支付CURD")
 public class PayController {
 
     @Resource
     private PayService payService;
 
     @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增", description = "新增支付流水")
     public String addPay(@RequestBody Pay pay){
         System.out.println(pay.toString());
         int i = payService.add(pay);
@@ -25,11 +29,13 @@ public class PayController {
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "删除", description = "删除支付流水")
     public Integer delitePay(@PathVariable("id") Integer id) {
         return payService.delete(id);
     }
 
     @PutMapping(value = "pay/update")
+    @Operation(summary = "修改", description = "修改支付流水")
     public String updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
@@ -39,11 +45,13 @@ public class PayController {
     }
 
     @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "查询单个", description = "查询单个支付流水")
     public Pay getById(@PathVariable("id") Integer id){
         return payService.getById(id);
     }
 
     @GetMapping(value = "/pay/list")
+    @Operation(summary = "查询所有", description = "查询所有支付流水")
     public List<Pay> getAll() {
         return payService.getAll();
     }
